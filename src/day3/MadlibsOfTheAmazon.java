e054f775a0031262a3d473cd2dcdc52bc5d53c43
Mrfuzzy <league@mini.222>
2015-11-25 16:51:17 -0800

    Robot stuff and maze

--- src/day3/MadlibsOfTheAmazon.java
+++ src/day3/MadlibsOfTheAmazon.java
@@ -7,16 +7,24 @@ public class MadlibsOfTheAmazon {
 
 		// Put this sentence in a pop up:
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
+		JOptionPane.showMessageDialog(null, "If you find yourself having to cross a piranha-infested river, here's how to do it...");
 
 		// Get the user to enter an adjective
+		JOptionPane.showInputDialog("Give me an adjective.");
 
 		// Get the user to enter a type of liquid
+		JOptionPane.showInputDialog("Give me a type of liquid.");
+		
+		
 
 		// Get the user to enter a body part
+		JOptionPane.showInputDialog("Give me a body part.");
 
 		// Get the user to enter a verb
+		JOptionPane.showInputDialog("Give me a verb.");
 
 		// Get the user to enter a place
+		JOptionPane.showInputDialog("Give me a place.");
 
 		// Fit the user's words into this sentence, and save it in a String:
 		// Piranhas are more [adjective] during the day, so cross the river at
